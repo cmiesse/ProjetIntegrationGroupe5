73fd3a66ea70d7eba33305de4c114fe8f6b571ca
BelgianNicky <riatnicky@hotmail.fr>
2019-10-14 00:04:15 +0200

    modif code leds

--- Arduino/sketch_oct10a/sketch_oct10a.ino
+++ Arduino/sketch_oct10a/sketch_oct10a.ino
@@ -1,19 +1,24 @@
 #include <FastLED.h>
 
-#define num_leds 5
+#define num_leds 60
 #define pin 6
 #define brightness 50     // can set 0-255
-#define sensitivity 5     // can set 0-10, recommended at 5
+#define sensitivity 1     // can set 0-10, recommended at 5
 CRGB leds[num_leds];
 
 void setup() {
-  FastLED.addLeds<WS2811, pin>(leds, num_leds);
+  FastLED.addLeds<WS2811, pin, GRB>(leds, num_leds);
   FastLED.setBrightness(brightness);
 }
 
 void loop() {
-  for(int i = 0; i < num_leds-1; i++) { 
-    leds[i] = CRGB(255,255,255); 
-  }
+  // Turn the first led red for 1 second
+  leds[0] = CRGB::Red; 
+  FastLED.show();
+  delay(1000);
+  
+  // Set the first led back to black for 1 second
+  leds[0] = CRGB::Black;
+  FastLED.show();
   delay(1000);
 }
