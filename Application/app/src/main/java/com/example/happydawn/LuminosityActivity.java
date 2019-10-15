8bf1d0e9d0b48ffe22134163e76b8623e6f26de3
SaitamEPHEC <m.gassmann@students.ephec.be>
2019-10-15 22:14:15 +0200

    Interface Page D'accueil
    - Interface
    - Boutons
    - Création des activités principales
    - Lien des boutons vers les activités

---
+++ Application/app/src/main/java/com/example/happydawn/LuminosityActivity.java
@@ -0,0 +1,14 @@
+package com.example.happydawn;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LuminosityActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_luminosity);
+    }
+}
--- Application/app/src/main/java/com/example/happydawn/MainActivity.java
+++ Application/app/src/main/java/com/example/happydawn/MainActivity.java
@@ -1,13 +1,56 @@
 package com.example.happydawn;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageButton luminosity;
+    private ImageButton alarm;
+    private ImageButton sound;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.luminosity = findViewById(R.id.luminosity);
+
+        luminosity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent otherActivity = new Intent(getApplicationContext(),LuminosityActivity.class);
+                startActivity(otherActivity);
+                finish();
+            }
+        });
+
+        this.alarm = findViewById(R.id.alarm);
+
+        alarm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent otherActivity = new Intent(getApplicationContext(),AlarmActivity.class);
+                startActivity(otherActivity);
+                finish();
+            }
+        });
+
+        this.sound = findViewById(R.id.sound);
+
+        sound.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent otherActivity = new Intent(getApplicationContext(),SoundActivity.class);
+                startActivity(otherActivity);
+                finish();
+            }
+        });
+
     }
 }
