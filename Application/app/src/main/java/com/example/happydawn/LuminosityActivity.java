e9e1eb8f65833d0c73aba149d4d2c8379eb27f97
AlexandreLefevre <44492979+AlexandreLefevre@users.noreply.github.com>
2019-10-16 18:13:11 +0200

    changement luminosite

--- Application/app/src/main/java/com/example/happydawn/LuminosityActivity.java
+++ Application/app/src/main/java/com/example/happydawn/LuminosityActivity.java
@@ -3,12 +3,39 @@ package com.example.happydawn;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ProgressBar;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class LuminosityActivity extends AppCompatActivity {
+    private TextView textview;
+    private SeekBar seekbar;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_luminosity);
+
+        textview = (TextView) findViewById(R.id.textview);
+        seekbar = (SeekBar) findViewById(R.id.seekBar);
+
+        seekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                textview.setText("" + progress + "%");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 }
