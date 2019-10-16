0b020d5dd7555c48bc1669bdbbd4fc7f8aaccb58
SaitamEPHEC <m.gassmann@students.ephec.be>
2019-10-16 20:52:39 +0200

    Interface Sound_activity terminée (premier jet)

--- Application/app/src/main/java/com/example/happydawn/SoundActivity.java
+++ Application/app/src/main/java/com/example/happydawn/SoundActivity.java
@@ -2,13 +2,33 @@ package com.example.happydawn;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class SoundActivity extends AppCompatActivity {
+
+    private Button retour;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sound);
+
+        this.retour = findViewById(R.id.retour);
+
+        retour.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent otherActivity = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(otherActivity);
+                finish();
+            }
+        });
     }
+
 }
