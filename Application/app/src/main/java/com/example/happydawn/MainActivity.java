b1423256a8d5b243921c9378898ad7565e5a5fde
SaitamEPHEC <m.gassmann@students.ephec.be>
2019-10-16 18:48:04 +0200

    layout mainActivity debug

--- Application/app/src/main/java/com/example/happydawn/MainActivity.java
+++ Application/app/src/main/java/com/example/happydawn/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.activity_main);
 
         this.luminosity = findViewById(R.id.luminosity);
 
