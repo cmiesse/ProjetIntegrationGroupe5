80750441749417b437eb48ccff2b606393b45cc0
cmiesse <c.miesse@students.ephec.be>
2019-11-04 14:41:21 +0100

    Ajout de PHPDocs pour les 2 classes

--- Web/INC/formHandler.php
+++ Web/INC/formHandler.php
@@ -1,6 +1,18 @@
 <?php
+
+/**
+ * Cette classe s'occupe de la manipulation de formulaires
+ */
   class FormHandler
   {
+      /**
+       * Vérifie que les 3 parametres sont remplis et que l'email a le bon format
+       * Retourne true si conditions remplies et false sinon
+       * @param string $nom le nom du contact
+       * @param string $mail l'adresse email du contact
+       * @param string $message le message de l'email
+       * @return boolean
+       */
       public function validateForm($nom, $mail, $message)
       {
           if (isset($nom) && isset($mail) && isset($message)) {
--- Web/tests/TestFormHandler.php
+++ Web/tests/TestFormHandler.php
@@ -2,8 +2,18 @@
 use PHPUnit\Framework\TestCase;
 require_once '../INC/formHandler.php';
 
+/**
+ * Cette classe est la classe de tests de la classe FormHandler
+ */
 class TestFormHandler extends TestCase
 {
+    /**
+     * Verifie que la méthode validateForm() retourne true dans le cas où
+     * nom = 'test'
+     * mail = 'test@test.be'
+     * message = 'Ceci est un test'
+     * @return void
+     */
     public function testValidateForm(){
         $object = new formHandler; //  setUp
 
