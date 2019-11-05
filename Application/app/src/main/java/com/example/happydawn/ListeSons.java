73512a9c050e1c82eeb1e7584111e8648593fcd1
SaitamEPHEC <m.gassmann@students.ephec.be>
2019-11-05 21:41:08 +0100

    Fonctionnalité choix de sons

---
+++ Application/app/src/main/java/com/example/happydawn/ListeSons.java
@@ -0,0 +1,37 @@
+package com.example.happydawn;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class ListeSons extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_liste_sons);
+
+       /* String[] items = {"Oiseau","Mer","Pluie","Coq"};
+        ListView listv = (ListView) findViewById(R.id.listeSons);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,R.layout.list_item,R.id.txt,items);
+        listv.setAdapter(adapter);
+        listv.setOnItemClickListener(new ItemList());*/
+
+    }
+
+   /* private class ItemList implements AdapterView.OnItemClickListener {
+
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            ViewGroup viewg = (ViewGroup) view;
+            TextView textv = (TextView) viewg.findViewById(R.id.txt);
+            Toast.makeText(ListeSons.this, textv.getText().toString(), Toast.LENGTH_LONG).show();
+        }
+    }*/
+}
--- Application/app/src/main/java/com/example/happydawn/SoundActivity.java
+++ Application/app/src/main/java/com/example/happydawn/SoundActivity.java
@@ -6,13 +6,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ProgressBar;
-import android.widget.SeekBar;
-import android.widget.TextView;
 
 public class SoundActivity extends AppCompatActivity {
 
     private Button retour;
+    private Button son;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +23,17 @@ public class SoundActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent otherActivity = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(otherActivity);
+                finish();
+            }
+        });
+
+        this.son = findViewById(R.id.son);
+
+        son.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent otherActivity = new Intent(getApplicationContext(),ListeSons.class);
                 startActivity(otherActivity);
                 finish();
             }
