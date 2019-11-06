4b9acf2cf5ec998fddb6ee7a5a0576f10f5e6a15
AlexandreLefevre <44492979+AlexandreLefevre@users.noreply.github.com>
2019-11-06 23:08:56 +0100

    Changement interface luminosite

--- Application/app/src/main/java/com/example/happydawn/LuminosityActivity.java
+++ Application/app/src/main/java/com/example/happydawn/LuminosityActivity.java
@@ -5,10 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.SeekBar;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class LuminosityActivity extends AppCompatActivity {
     private TextView textview;
@@ -19,6 +22,16 @@ public class LuminosityActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_luminosity);
+
+        Spinner spinner = findViewById(R.id.spinner1);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.couleur, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        Spinner spinner2 = findViewById(R.id.spinner2);
+        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this, R.array.minute2, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
 
         textview = (TextView) findViewById(R.id.textview);
         seekbar = (SeekBar) findViewById(R.id.seekBar);
