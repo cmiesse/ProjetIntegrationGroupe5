9edb546cfb0247355f1f38166b4d15ebb2274baf
cmiesse <c.miesse@students.ephec.be>
2019-11-07 10:27:10 +0100

    Ajout d'autres tests unitaires sur validateForm

--- Web/tests/TestFormHandler.php
+++ Web/tests/TestFormHandler.php
@@ -14,11 +14,87 @@ class TestFormHandler extends TestCase
      * message = 'Ceci est un test'
      * @return void
      */
-    public function testValidateForm(){
+    public function testValidateFormAllParametersAndValidEmail(){
         $object = new formHandler; //  setUp
 
         $functionResult = $object->validateForm('test', 'test@test.be', 'Ceci est un test');  // Exercice
 
         $this->assertSame(true, $functionResult );   // Assert
     }
+
+    /**
+     * Verifie que la méthode validateForm() retourne false dans le cas où
+     * nom = null
+     * mail = null
+     * message = null
+     * @return void
+     */
+    public function testValidateFormNoParameters(){
+        $object = new formHandler; //  setUp
+
+        $functionResult = $object->validateForm(null,null,null);  // Exercice
+
+        $this->assertSame(false, $functionResult );   // Assert
+    }
+
+    /**
+     * Verifie que la méthode validateForm() retourne false dans le cas où
+     * nom = null
+     * mail = 'test@test.be'
+     * message = 'Ceci est un test'
+     * @return void
+     */
+    public function testValidateFormAllParametersButNameMissing(){
+        $object = new formHandler; //  setUp
+
+        $functionResult = $object->validateForm(null, 'test@test.be', 'Ceci est un test');  // Exercice
+
+        $this->assertSame(false, $functionResult );   // Assert
+    }
+
+    /**
+     * Verifie que la méthode validateForm() retourne false dans le cas où
+     * nom = 'test'
+     * mail = null
+     * message = 'Ceci est un test'
+     * @return void
+     */
+    public function testValidateFormAllParametersButEmailMissing(){
+        $object = new formHandler; //  setUp
+
+        $functionResult = $object->validateForm('test', null, 'Ceci est un test');  // Exercice
+
+        $this->assertSame(false, $functionResult );   // Assert
+    }
+
+    /**
+     * Verifie que la méthode validateForm() retourne false dans le cas où
+     * nom = 'test'
+     * mail = 'test@test.be'
+     * message = null
+     * @return void
+     */
+    public function testValidateFormAllParametersButMessageMissing(){
+        $object = new formHandler; //  setUp
+
+        $functionResult = $object->validateForm('test', 'test@test.be', null);  // Exercice
+
+        $this->assertSame(false, $functionResult );   // Assert
+    }
+
+    /**
+     * Verifie que la méthode validateForm() retourne false dans le cas où
+     * nom = 'test'
+     * mail = 'test+test.be'
+     * message = 'Ceci est un test'
+     * @return void
+     */
+    public function testValidateFormAllParametersAndWrongEmailFormat(){
+        $object = new formHandler; //  setUp
+
+        $functionResult = $object->validateForm('test', 'test+test.be','Ceci est un test');  // Exercice
+
+        $this->assertSame(false, $functionResult );   // Assert
+    }
+
 }
