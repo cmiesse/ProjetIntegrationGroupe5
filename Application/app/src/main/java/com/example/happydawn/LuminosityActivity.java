6ce606e80f6b2481f2d659de29c204542163199f
AlexandreLefevre <44492979+AlexandreLefevre@users.noreply.github.com>
2019-11-14 09:26:44 +0100

    Bluetooth main activity
    Avancement sur le bluetooth

--- Application/app/src/main/java/com/example/happydawn/LuminosityActivity.java
+++ Application/app/src/main/java/com/example/happydawn/LuminosityActivity.java
@@ -17,18 +17,12 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class LuminosityActivity extends AppCompatActivity {
-    private static final int REQUEST_ENABLE_BT = 0;
-    private static final int REQUEST_DISCOVER_BT = 1;
     private TextView textview;
-    private SeekBar seekbar;
-    private Button retour;
-    private int luminosite;
-    private String couleur;
-    private String duree;
-    private ImageView mBluetooth;
-    private Button monbutton,moffbutton;
-    private BluetoothAdapter bluetoothAdapter;
-
+    SeekBar seekbar;
+    Button retour;
+    int luminosite;
+    String couleur;
+    String duree;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,41 +43,6 @@ public class LuminosityActivity extends AppCompatActivity {
 
         textview = (TextView) findViewById(R.id.textview);
         seekbar = (SeekBar) findViewById(R.id.seekBar);
-        moffbutton = findViewById(R.id.buttonoff);
-        monbutton = findViewById(R.id.buttonon);
-        //adaptateur
-        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-        // set image according to bluetooth statues(on/off)
-        if (bluetoothAdapter.isEnabled()) {
-            mBluetooth.setImageResource(R.drawable.ic_action_on);
-        } else {
-            mBluetooth.setImageResource(R.drawable.ic_action_of);
-        }
-        monbutton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (!bluetoothAdapter.isEnabled()) {
-                    showtoast("Turning bluetooth on...");
-                    Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-                    startActivityForResult(intent, REQUEST_ENABLE_BT);
-                } else {
-                    showtoast("Bluetooth is already on");
-                }
-            }
-        });
-        moffbutton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (bluetoothAdapter.isEnabled()){
-                    bluetoothAdapter.disable();
-                    showtoast("Turning Bluetooth Off");
-                    mBluetooth.setImageResource(R.drawable.ic_action_of);
-                }
-                else {
-                    showtoast("Bluetooth is already off");
-                }
-            }
-        });
 
         seekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
@@ -115,26 +74,6 @@ public class LuminosityActivity extends AppCompatActivity {
         });
 
     }
-
-
-        @Override
-        protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-            switch (requestCode){
-                case REQUEST_ENABLE_BT:
-                    if (resultCode == RESULT_OK){
-                        //bluetooth is on
-                        mBluetooth.setImageResource(R.drawable.ic_action_on);
-                        showtoast("Bluetooth is on");
-                    }
-                    else {
-                        //user denied to turn bluetooth on
-                        showtoast("could't on bluetooth");
-                    }
-                    break;
-            }
-            super.onActivityResult(requestCode, resultCode, data);
-        }
-
         private void showtoast(String msg){
         Toast.makeText(this,msg,Toast.LENGTH_SHORT).show();
     }
--- Application/app/src/main/java/com/example/happydawn/MainActivity.java
+++ Application/app/src/main/java/com/example/happydawn/MainActivity.java
@@ -2,11 +2,16 @@ package com.example.happydawn;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.content.Intent;
 import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.Toast;
+
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,6 +23,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        if(bluetoothAdapter == null){
+            Toast.makeText(getApplicationContext(),"Device doesn't Support Bluetooth",Toast.LENGTH_SHORT).show();
+        }
+        if(!bluetoothAdapter.isEnabled()){
+            Intent enableAdapter = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+
+            startActivityForResult(enableAdapter,0);
+        }
+
+        Set bondedDevices = bluetoothAdapter.getBondedDevices();
+        if(bondedDevices.isEmpty()){
+            Toast.makeText(getApplicationContext(),"Please pair the devices first",Toast.LENGTH_SHORT).show();
+        }else{
+            for (BluetoothDevice iterator : bondedDevices) {
+
+            if(iterator.getAddress().equals(DEVICE_ADDRESS)) //Replace with iterator.getName() if comparing Device names.
+
+            {
+
+                device=iterator; //device is an object of type BluetoothDevice
+
+                found=true;
+
+                break;
+
+            } } }
 
         this.luminosity = findViewById(R.id.luminosity);
 
