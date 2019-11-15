a5060bf35efdcbdcd1edd1bebb0d4a34782d9af4
cmiesse <c.miesse@students.ephec.be>
2019-11-15 15:46:17 +0100

    Correction Bugs Achat et Confirmation

--- Web/Purchase.php
+++ Web/Purchase.php
@@ -6,11 +6,11 @@
         if($myForm->validatePurchaseForm($_POST['name'], $_POST['mail'], $_POST['quantity'], $_POST['address'], $_POST['city'], $_POST['country'])){
           $to = "contact@happydawn.be"; // this is your Email address
           $from = $_POST['mail']; // this is the sender's Email address
-          $nameFrom = utf8_decode($_POST['name']);
+          $nameFrom = $_POST['name'];
           $subject = "Formulaire d'achat";
-          $message = $nameFrom . ' souhaiterait achater ' . $_POST['quantity'] . ' réveil(s) \n
-           Son adresse est : ' . utf8_decode($_POST['address']) . ' à ' . utf8_decode($_POST['city']). ' en ' . utf8_decode($_POST['country']) . '.\n
-           Son adresse mail est ' . $_POST['mail'];
+          $message = $nameFrom . " souhaiterait acheter " . $_POST['quantity'] . " réveil(s).\n
+           Son adresse est : " . $_POST['address'] . " à " . $_POST['city']. " en " . $_POST['country'] . ".\n
+           Son adresse mail est " . $_POST['mail'];
           $headers = "From: " . $from."\n";
           $headers .="Content-type: text/html; charset=utf8\n";
           mail($to,$subject,$message,$headers);
@@ -33,7 +33,7 @@
     <title>HappyDawn - Achat</title>
   </head>
   <body style="background-color:gray">
-    <a class="w3-button w3-teal w3-padding-large" href="www.happydawn.be">Retour à l'accueil</a><br>
+    <a class="w3-button w3-teal w3-padding-large" href="index.php">Retour à l'accueil</a><br>
     <h1 style="margin-left:20px;">Acheter notre réveil</h1>
     <div class="w3-col m7">
       <form class="w3-container w3-card-4 w3-padding-16 w3-white" action="" method="post" style="margin-left:20px;">
