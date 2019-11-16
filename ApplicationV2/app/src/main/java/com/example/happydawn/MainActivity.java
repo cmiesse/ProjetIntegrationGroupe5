ecffe49495df1b8e4924e24010fed46aed608624
AlexandreLefevre <44492979+AlexandreLefevre@users.noreply.github.com>
2019-11-16 16:40:07 +0100

    Api meteo debut
    a continuer

--- ApplicationV2/app/src/main/java/com/example/happydawn/MainActivity.java
+++ ApplicationV2/app/src/main/java/com/example/happydawn/MainActivity.java
@@ -11,11 +11,16 @@ import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import org.json.JSONObject;
 
 import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView t1_degree,t2_temps;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +29,15 @@ public class MainActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
+        t1_degree = (TextView)findViewById(R.id.degre);
+        t2_temps = (TextView)findViewById(R.id.temps);
+
+        findwethear();
+    }
+
+    public void findwethear(){
+        String url = "";
+        
     }
 
     public void onClickStart(View view) {
