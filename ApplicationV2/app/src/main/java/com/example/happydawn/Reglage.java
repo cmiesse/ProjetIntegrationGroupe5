28271b1b56e92e890eeab0e0228a3727c500d9de
GillesTimothy <44493111+GillesTimothy@users.noreply.github.com>
2019-11-20 22:12:08 +0100

    ajout popup

--- ApplicationV2/app/src/main/java/com/example/happydawn/Reglage.java
+++ ApplicationV2/app/src/main/java/com/example/happydawn/Reglage.java
@@ -6,11 +6,15 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.PopupMenu;
+import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.TimePicker;
+import android.widget.Toast;
 
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
@@ -21,6 +25,15 @@ public class Reglage extends AppCompatActivity {
 
     private ImageView retour;
     private Button son;
+    private Button luminosite;
+    private Button couleurs;
+    private Button duree;
+    private Button augmentation;
+    private Button libelle;
+    private TextView textvolume;
+    int volume;
+    SeekBar seekbar;
+
     AlarmManager alarm_manager;
     TimePicker timePicker;
     Context context;
@@ -37,18 +50,18 @@ public class Reglage extends AppCompatActivity {
         retour.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent otherActivity = new Intent(getApplicationContext(),Alarme.class);
+                Intent otherActivity = new Intent(getApplicationContext(), Alarme.class);
                 startActivity(otherActivity);
                 finish();
             }
         });
 
-        this.son= findViewById(R.id.son);
+        this.son = findViewById(R.id.son);
 
         son.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent otherActivity = new Intent(getApplicationContext(),ListeSons.class);
+                Intent otherActivity = new Intent(getApplicationContext(), ListeSons.class);
                 startActivity(otherActivity);
                 finish();
             }
@@ -60,10 +73,10 @@ public class Reglage extends AppCompatActivity {
         alarm_manager = (AlarmManager) getSystemService(ALARM_SERVICE);
 
         //initialise le timePicker
-        timePicker = (TimePicker) findViewById(R.id.timePicker);
+        timePicker = findViewById(R.id.timePicker);
 
         //initialise text update
-        updateText = (TextView) findViewById(R.id.updateText);
+        updateText = findViewById(R.id.updateText);
 
         final Calendar calendar = Calendar.getInstance();
 
@@ -87,24 +100,123 @@ public class Reglage extends AppCompatActivity {
                 String hour_string = String.valueOf(hour);
                 String minute_string = String.valueOf(minute);
 
-                if(hour>12){
+                if (hour > 12) {
                     hour_string = String.valueOf(hour - 12);
                 }
 
-                if(minute < 10){
+                if (minute < 10) {
                     minute_string = String.valueOf("0" + minute);
                 }
 
-                set_alarm_text(" -> Alarme ON" +"\n          "+ hour_string + ":" + minute_string);
+                set_alarm_text(" -> Alarme ON" + "\n          " + hour_string + ":" + minute_string);
 
                 //pending intent
                 pending_intent = PendingIntent.getBroadcast(Reglage.this, 0, my_intent
                         , PendingIntent.FLAG_UPDATE_CURRENT);
 
                 //set alarm manager
-                alarm_manager.set(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(),pending_intent);
+                alarm_manager.set(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pending_intent);
             }
         });
+
+
+        textvolume = findViewById(R.id.textvolume);
+        seekbar = findViewById(R.id.seekBar);
+
+        seekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                textvolume.setText(getString(R.string.volume, progress));
+                volume = progress;
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        luminosite = findViewById(R.id.luminosite);
+        luminosite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PopupMenu popup_luminosite = new PopupMenu(Reglage.this,luminosite);
+                popup_luminosite.getMenuInflater().inflate(R.menu.popup_luminosite, popup_luminosite.getMenu());
+
+                popup_luminosite.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        Toast.makeText(Reglage.this, "" + item.getTitle(), Toast.LENGTH_SHORT).show();
+                        return true;
+                    }
+                });
+
+                popup_luminosite.show();
+            }
+        });
+
+        couleurs = findViewById(R.id.couleurs);
+        couleurs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PopupMenu popup_couleurs = new PopupMenu(Reglage.this,couleurs);
+                popup_couleurs.getMenuInflater().inflate(R.menu.popup_couleur, popup_couleurs.getMenu());
+
+                popup_couleurs.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        Toast.makeText(Reglage.this, "" + item.getTitle(), Toast.LENGTH_SHORT).show();
+                        return true;
+                    }
+                });
+
+                popup_couleurs.show();
+            }
+        });
+
+        duree = findViewById(R.id.duree);
+        duree.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PopupMenu popup_duree = new PopupMenu(Reglage.this,duree);
+                popup_duree.getMenuInflater().inflate(R.menu.popup_duree, popup_duree.getMenu());
+
+                popup_duree.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        Toast.makeText(Reglage.this, "" + item.getTitle(), Toast.LENGTH_SHORT).show();
+                        return true;
+                    }
+                });
+
+                popup_duree.show();
+            }
+        });
+
+        augmentation = findViewById(R.id.augmentation);
+        augmentation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PopupMenu popup_augmentation = new PopupMenu(Reglage.this,augmentation);
+                popup_augmentation.getMenuInflater().inflate(R.menu.popup_augmentation, popup_augmentation.getMenu());
+
+                popup_augmentation.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        Toast.makeText(Reglage.this, "" + item.getTitle(), Toast.LENGTH_SHORT).show();
+                        return true;
+                    }
+                });
+
+                popup_augmentation.show();
+            }
+        });
+
 
     }
 
