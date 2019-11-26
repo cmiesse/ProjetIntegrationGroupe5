052b5be26c02ecb098d569fdfe097666a52a63cb
cmiesse <c.miesse@students.ephec.be>
2019-11-27 00:17:28 +0100

    Début adaptation CSS + flèche de bas de page vers haut enlevé

--- Web/Purchase.php
+++ Web/Purchase.php
@@ -27,9 +27,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-    <link rel="stylesheet" href="https://www.w3schools.com/lib/w3-theme-black.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="CSS/w3.css">
     <link rel="icon" href="soleil.ico" />
     <title><?php echo TXT_TITLE_BUY ?></title>
   </head>
--- Web/index.php
+++ Web/index.php
@@ -43,9 +43,7 @@
 <title>HappyDawn</title>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-<link rel="stylesheet" href="https://www.w3schools.com/lib/w3-theme-black.css">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="CSS/w3.css">
 <link rel="icon" href="soleil.ico" />
 <body id="myPage">
 
@@ -230,11 +228,7 @@
 
 <!-- Pied de page -->
 <footer class="w3-container w3-padding-32 w3-theme-d1 w3-center">
-  <div style="position:relative;bottom:25px;z-index:1;" class="w3-tooltip w3-right">
-    <span class="w3-text w3-padding w3-teal w3-hide-small">Revenir en haut</span>
-    <a class="w3-button w3-theme" href="#myPage"><span class="w3-xlarge">
-    <i class="fa fa-chevron-circle-up"></i></span></a>
-  </div>
+
 
   <span><?php echo TXT_FOOTER ?></span>
 
