036b310c28fdcf2b4699aa51e1030c357d156bdd
arnogodart95 <44059862+arnogodart95@users.noreply.github.com>
2019-12-04 17:40:58 +0100

    Update creerAlarme.java

--- ApplicationV2/app/src/main/java/com/example/happydawn/creerAlarme.java
+++ ApplicationV2/app/src/main/java/com/example/happydawn/creerAlarme.java
@@ -1,6 +1,20 @@
 package com.example.happydawn;
 
 public class creerAlarme {
+        private int seconde;
 
-    
+
+
+
+
+
+
+
+        public void setSeconde(int seconde) {
+        this.seconde = seconde;
+    }
+
+    public int getSeconde() {
+        return seconde;
+    }
 }
